52d7f6a6762a85b26a74a6f336b8082177914278
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2018-03-27 22:42:03 +0200

    deleted old models file, got rid of createsuperuser command, added tokenize expressions command

--- ComSemApp/management/commands/createsu.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("admin", "zekehuntergreen@gmail.com", "1965%Bridjam")
