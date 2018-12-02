d4df31d28980ff2b5bb25b9a0984cb69cfd66b40
Daniel Richard <daniel.richard@outlook.com>
2018-12-02 12:07:11 -0800

    -update

--- ComSemApp/administrator/views.py
+++ ComSemApp/administrator/views.py
@@ -127,7 +127,7 @@ class StudentListView(AdminViewMixin, ListView):
                         dupeUser = True
                         print(user.user)
                         print("DUPE USER")
-                        message.append(count+ " " + fields[0]+" "+fields[1]+" "+fields[2]+"    Duplicate Username)
+                        message.append(count+ " " + fields[0]+" "+fields[1]+" "+fields[2]+"    Duplicate Username")
                         break
                 if (dupeUser == True):
                     #end of file
