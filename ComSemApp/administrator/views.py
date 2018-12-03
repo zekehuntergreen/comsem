fb00ec9ae0aab8aa27f463463cef71d8ed954cb0
Daniel Richard <daniel.richard@outlook.com>
2018-12-02 17:33:05 -0800

    update

--- ComSemApp/administrator/views.py
+++ ComSemApp/administrator/views.py
@@ -115,9 +115,9 @@ class StudentListView(AdminViewMixin, ListView):
             lines = file_data.split("\n")
             rejectedLines = []
             message_content = ["The Following users were not added: \n"]
-            
+            line = 0
             for line in lines:
-                count = 2
+                line = line + 1
                 if len(line): #make sure line isnt empty
                     fields = line.split(",")
                     okToCreate = True
@@ -125,13 +125,13 @@ class StudentListView(AdminViewMixin, ListView):
                         #end of file
                         break
                     if (fields[0].isalpha() == False or fields[1].isalpha() == False):
-                        message = ( fields[0] + " " + fields[1] + " " + fields[2] + "    Invalid First or Last Name \n")
+                        message = (line + " " fields[0] + " " + fields[1] + " " + fields[2] + "    Invalid First or Last Name \n")
                         message_content.append(message)
                         okToCreate = False
                     for user in Student.objects.filter(institution=self.institution):
                         if(user.user.username== fields[2]):
                             okToCreate = False
-                            message = ( fields[0] + " " + fields[1] + " " + fields[2] + "    Duplicate Email Address \n")
+                            message = (line + " " fields[0] + " " + fields[1] + " " + fields[2] + "    Duplicate Email Address \n")
                             message_content.append(message)
                             break
                     
@@ -139,7 +139,7 @@ class StudentListView(AdminViewMixin, ListView):
                     match = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', fields[2])
                     if (match == None):
                         okToCreate = False 
-                        message = ( fields[0] + " " + fields[1] + " " + fields[2] + "    Invalid Email Address \n")
+                        message = (line + " " fields[0] + " " + fields[1] + " " + fields[2] + "    Invalid Email Address \n")
                         message_content.append(message)
                     user = {
                         "first_name": fields[0],
