0afb70782f87bda3608af275cb623eba83cd1ace
Daniel Richard <daniel.richard@outlook.com>
2019-02-10 15:40:53 -0800

    update

--- ComSemApp/teacher/views.py
+++ ComSemApp/teacher/views.py
@@ -78,14 +78,14 @@ class CourseDetailView(TeacherCourseViewMixin, DetailView):
         worksheets = Worksheet.objects.filter(course=self.course)
         subcountdict = {}
         for student in self.course.students.all(): 
-            print(student.user.username)
+
             subcount = 0
             submissions = StudentSubmission.objects.filter(student=student)
             for submission in submissions :
                 subcount = subcount + 1
                 if submission.worksheet.course != self.course:
                     subcount = subcount - 1
-            subcountdict[student] = subcount
+            subcountdict[student.user.username] = subcount
         
         
         count = len(worksheets)
