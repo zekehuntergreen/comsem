9088883ba3284d3d0ed3a2c33d41a8951bb8a8c3
Daniel Richard <daniel.richard@outlook.com>
2019-02-18 15:48:38 -0800

    update

--- ComSemApp/teacher/views.py
+++ ComSemApp/teacher/views.py
@@ -97,7 +97,7 @@ class CourseDetailView(TeacherCourseViewMixin, DetailView):
             for submission in submissions: 
                 print("num of attempts:")
                 print(submission.get_number())
-                print(submission.student)
+                print(submission.worksheet.course)
             subcountdict[student.user.username] = subcount
             ungradedcountdict[student.user.username] = ungradedcount
 
