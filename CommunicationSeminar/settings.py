e24a4b3957d9e99e6a68f53723c29911573cf86d
tmgreenside <trevmg7@gmail.com>
2019-03-25 15:26:35 -0700

    New feature branch

--- CommunicationSeminar/settings.py
+++ CommunicationSeminar/settings.py
@@ -104,7 +104,7 @@ else:
             'PORT': '3306',
             'NAME': 'CommunicationSeminarDjango',
             'USER': 'root',
-            'PASSWORD': '2017%ComSem',
+            'PASSWORD': 'tempcbsql',
         }
     }
 
