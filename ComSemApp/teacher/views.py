b537f2beecfc85c0d1decba0d41500500c4dc638
Daniel Richard <daniel.richard@outlook.com>
2019-04-28 13:32:15 -0700

    update

--- ComSemApp/teacher/views.py
+++ ComSemApp/teacher/views.py
@@ -93,8 +93,8 @@ class CourseDetailView(TeacherCourseViewMixin, DetailView):
                     if submission.status == 'ungraded':
                         ungradedcount = ungradedcount + 1
 
-            for worksheet in worksheets.all():
-                if worksheet.last_submission(student):
+            for worksheet in worksheets:
+                if worksheet.last_submission(self.student):
                     attemptcount = attemptcount + worksheet.last_submission(student).get_number()
                 if worksheet.auto_student == self.student or worksheet.auto_student == None:
                     worksheetcount = worksheetcount + 1
