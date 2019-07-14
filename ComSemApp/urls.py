8e98a1de9712dcfa209866f60f57c986dafe70f5
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2019-07-14 15:33:12 -0700

    implemented l1 and l2 tests

--- ComSemApp/urls.py
+++ ComSemApp/urls.py
@@ -6,8 +6,6 @@ urlpatterns = [
     url(r'^$', views.about, name='about'),
     url(r'^about/teacher/$', views.AboutTeacher.as_view(), name='about_teacher'),
 
-    url(r'^error-recognition/', include('error_recognition.urls', namespace='error_recognition')),
-
     url(r'^initiate_roles/$', views.initiate_roles, name='initiate_roles'),
     url(r'^administrator/', include('ComSemApp.administrator.urls', namespace='administrator')),
     url(r'^teacher/', include('ComSemApp.teacher.urls', namespace='teacher')),
--- error_recognition/urls.py
+++ error_recognition/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 app_name = "error_recognition"
 urlpatterns = [
-    path('', views.ErrorRecognitionTestWelcome.as_view(), name='welcome'),
+    path('error-recognition/', views.WelcomeView.as_view(), name='welcome'),
+    path('l1', views.TestL1View.as_view(), name='l1'),  # no trailing slash !
+    path('l2', views.TestL2View.as_view(), name='l2'),  # same !
 ]
