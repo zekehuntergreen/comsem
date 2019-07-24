7eb876b7e731474e193d5338d93cd40fba4df1ec
Zeke Hunter-Green <zekehuntergreen@gmail.com>
2019-07-24 11:28:38 +0100

    delete STATICFILES_DIRS

--- CommunicationSeminar/settings/production.py
+++ CommunicationSeminar/settings/production.py
@@ -224,14 +224,8 @@ LOGGING = {
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-# Extra lookup directories for collectstatic to find static files
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 #  Add configuration for static files storage using whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
