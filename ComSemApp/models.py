93834553c281ab6d89e1cf0ab62df96b338b7bf2
Zeke Hunter-Green <zeke@wagestream.co.uk>
2020-05-01 19:39:36 +0100

    remove reviewattempt.course

--- ComSemApp/models.py
+++ ComSemApp/models.py
@@ -276,7 +276,6 @@ class StudentAttempt(models.Model):
 
 class ReviewAttempt(models.Model):
     expression = models.ForeignKey('Expression', on_delete=models.CASCADE)
-    course = models.ForeignKey('Course', on_delete=models.CASCADE)
     student = models.ForeignKey('Student', on_delete=models.SET_NULL, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
     correct = models.BooleanField(default=None)
