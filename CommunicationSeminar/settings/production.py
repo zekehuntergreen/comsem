6dcbe01654c89c0e0d0d7e57a4393fc21f664682
Zeke Hunter-Green <zeke.huntergreen@guardian.co.uk>
2022-09-18 11:45:01 +0100

    set AWS_DEFAULT_ACL

--- CommunicationSeminar/settings/production.py
+++ CommunicationSeminar/settings/production.py
@@ -261,6 +261,7 @@ if LIVE:
     AWS_ACCESS_KEY_ID = env_get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = env_get('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = env_get('AWS_STORAGE_BUCKET_NAME')
+    AWS_DEFAULT_ACL = env_get('AWS_DEFAULT_ACL')
     AWS_S3_OBJECT_PARAMETERS = {
         'CacheControl': 'max-age=86400',
     }
