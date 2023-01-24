7325a6e02290ae7f5426d78151f84b667e971539
Avery Edmondson <edmondsonavery@gmail.com>
2023-01-24 15:13:40 -0800

    added type-hinting and docstrings

--- ComSemApp/tests/test_models.py
+++ ComSemApp/tests/test_models.py
@@ -9,12 +9,20 @@ from ComSemApp.libs.factories import BaseTestCase
 
 class TestSpeakingPracticeAttempt(BaseTestCase):
     """
-        Creates a test 
+        Test class for the SpeakingPracticeAttempt database model
+        Inherits from a custom BaseTestCase class with extra methods for easy database manipulation
     """
-    def setUp(self):
+    def setUp(self) -> None:
+        """
+            Implementation of standard setUp function.
+            Creates one instance of Expression for the SpeakingPracticeAttempts to reference
+        """
         self.expression = self.db_create_expression()
     
-    def test_exception_on_null_fields(self):
+    def test_exception_on_null_fields(self) -> None:
+        """
+            Tests that an exception is thrown when any Not Null field is undefined
+        """
         with transaction.atomic():
             self.assertRaises(IntegrityError, SpeakingPracticeAttempt.objects.create)
         with transaction.atomic():
@@ -30,10 +38,16 @@ class TestSpeakingPracticeAttempt(BaseTestCase):
         with transaction.atomic():
             self.assertRaises(IntegrityError, SpeakingPracticeAttempt.objects.create, wpm=100)
 
-    def test_no_error_with_allowed_fields_null(self):
+    def test_no_error_with_allowed_fields_null(self) -> None:
+        """
+            Tests that an exception is not thrown when fields which are allowed to be null are undefined
+        """
         self.assertIsNotNone(SpeakingPracticeAttempt.objects.create(expression=self.expression, correct=90, wpm=100))
 
-    def test_exception_on_out_of_range_values(self):
+    def test_exception_on_out_of_range_values(self) -> None:
+        """
+            Tests that exceptions are thrown when validating values that are out of range
+        """
         # wpm too high
         with transaction.atomic():
             self.assertRaises(InvalidOperation, SpeakingPracticeAttempt.objects.create, expression=self.expression, correct=90, wpm=1000)
