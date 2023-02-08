73ea34856ff6381e26cc68141f183d757da402f9
zambroseo <57931459+zambroseo@users.noreply.github.com>
2023-02-08 13:50:42 -0800

    Comments and formatting Update

--- grading_algorithm.py
+++ grading_algorithm.py
@@ -14,37 +14,41 @@ def grade_expression(expression, c_exp) :
     if correct_expression(expression, c_exp) == True:
         return 100
 
-    expression_list = expression.split()
     c_exp_list = c_exp.split()
 
+    # Begins with checking the position of words
     total_order_score = 0
     total_word_count = len(c_exp_list)
     position_error_count = word_position(expression, c_exp)
+
+    # Math for the algorithm, converts it to a percentage and then converts it to 20% of the overall grade
     position_percent = ((total_word_count - position_error_count) / total_word_count) * 100
-    position_percent_of_order_score= position_percent * .2
+    position_percent_of_order_score = position_percent * .2
 
+    # Adds to total
     total_order_score = total_order_score + position_percent_of_order_score
+    # Word count check 
     if word_count(expression, c_exp) == True:
+        # Add 10% to the overrall order score if the word count matches
         total_order_score = total_order_score + 10
-    print(total_order_score)
 
+    # Performs the word order check
     order_error_count = word_order(expression, c_exp)
+    
+    # Once again converts to percent, but this section is instead worth 70% of the order portion
     order_percent = ((total_word_count - order_error_count) / total_word_count) * 100
-    print(order_percent)
     order_percent_of_total = order_percent * .7
     total_order_score = total_order_score + order_percent_of_total
 
-    print(total_order_score)
-
+    # The presence check
     presence_error_count = word_prescence(expression, c_exp)
+
+    # Converts presence score to a percent which is the overrall score for the presence portion
     presence_percent = ((total_word_count - presence_error_count) / total_word_count) 
     presence_score = presence_percent * 100
 
-    print(presence_score)
-    grade = 0 
+    # Combines the two sections, both being weighted at 50%
     grade = (total_order_score * .5) + (presence_score * .5)
-    print(grade)
-
     return grade
 
 def correct_expression(expression, c_exp):
@@ -72,7 +76,6 @@ def word_prescence(expression, c_exp):
         error_count(int): the amount of times that a word is not present
     """
     c_exp_list = c_exp.split()
-    expression_list = expression.split()
     error_count = 0
 
     #Very simple checks if all correct words are present
@@ -96,18 +99,18 @@ def word_order(expression, c_exp):
     c_exp_list = c_exp.split()
     error_count = 0
     for x in range(len(expression_list)):
-        if (x + 1) < len(expression_list): # makes sure that it will not go out of index checking the next word
-            if expression_list[x] in c_exp_list: #checks if the word is in the correct expression
-                word_index = c_exp_list.index(expression_list[x]) #sets word index equal to the index of the word in the orignal expression
-                if (word_index + 1) < len(c_exp_list): #make sure that there is a word after the desired word
-                    if expression_list[x+1] == c_exp_list[word_index+1]: #checks if that word matches the next word in the expression
+        if (x + 1) < len(expression_list): # Makes sure that it will not go out of index checking the next word
+            if expression_list[x] in c_exp_list: # Checks if the word is in the correct expression
+                word_index = c_exp_list.index(expression_list[x]) # Sets word index equal to the index of the word in the orignal expression
+                if (word_index + 1) < len(c_exp_list): # Make sure that there is a word after the desired word
+                    if expression_list[x+1] == c_exp_list[word_index+1]: # Checks if that word matches the next word in the expression
                         continue
                     else:
                         error_count += 1
 
-        if x == len(expression_list)-1: # if the index is to the last word in expression, since the last word has no word after it
-            word_index = c_exp_list.index(expression_list[x]) # gets the index of the word in the correct expression
-            if word_index == len(c_exp_list)-1: # checks if the word is the last word in the correct expression
+        if x == len(expression_list)-1: # If the index is to the last word in expression, since the last word has no word after it
+            word_index = c_exp_list.index(expression_list[x]) # Gets the index of the word in the correct expression
+            if word_index == len(c_exp_list)-1: # Checks if the word is the last word in the correct expression
                 continue
             else:
                 error_count += 1
@@ -127,7 +130,7 @@ def word_position(expression, c_exp):
     expression_list = expression.split()
     c_exp_list = c_exp.split()
     error_count = 0
-    # checks if the postiion of a word is the same in each expression
+    # Checks if the postiion of a word is the same in each expression
     for x in range(len(expression_list)):
         if x < len(c_exp_list):
             if expression_list[x] != c_exp_list[x]:
@@ -155,35 +158,6 @@ def word_count(expression, c_exp):
 expression = "Hello my name pineapple Elder Price"
 print(expression)
 c_exp = "Hello my name is Elder Price"
-print(correct_expression(expression, c_exp))
 print("Presence error count:", word_prescence(expression, c_exp))
-print("Word order error count:", word_order(expression, c_exp))
-
-expression_list = expression.split()
-c_exp_list = c_exp.split()
-
-total_order_score = 0
-total_word_count = len(c_exp_list)
-position_error_count = word_position(expression, c_exp)
-position_percent = ((total_word_count - position_error_count) / total_word_count) * 100
-position_percent_of_order_score= position_percent * .2
-
-total_order_score = total_order_score + position_percent_of_order_score
-if word_count(expression, c_exp) == True:
-    total_order_score = total_order_score + 10
-
-order_error_count = word_order(expression, c_exp)
-order_percent = ((total_word_count - order_error_count) / total_word_count) * 100
-order_percent_of_total = order_percent * .7
-total_order_score = total_order_score + order_percent_of_total
-
-print(total_order_score)
-
-presence_error_count = word_prescence(expression, c_exp)
-presence_percent = ((total_word_count - presence_error_count) / total_word_count) 
-presence_score = presence_percent * 100
-
-print(presence_score)
-grade = 0 
-grade = (total_order_score * .5) + (presence_score * .5)
-print(grade)
+print("Word order error count:", word_order(expression, c_exp) + word_position(expression, c_exp))
+print("Your overall grade was:", grade_expression(expression, c_exp))
