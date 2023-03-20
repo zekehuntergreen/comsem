41b820fdb7ed15585337aac647d6f8483c58c738
Avery Edmondson <edmondsonavery@gmail.com>
2023-03-19 17:34:12 -0700

    added some more type hinting and documentation

--- ComSemApp/libs/factories.py
+++ ComSemApp/libs/factories.py
@@ -12,7 +12,7 @@ from ComSemApp.models import *
 from django.test import TestCase
 from django.test import Client
 
-class UserParams(TypedDict, total=False):
+class FactoryUserParams(TypedDict, total=False):
     username : str
     password : str
 
@@ -39,9 +39,13 @@ class Factory:
         self._institution = Institution.objects.create(**defaults)
         return self._institution
 
-    def db_create_user(self, **kwargs : Unpack[UserParams]) -> User:
+    def db_create_user(self, **kwargs : Unpack[FactoryUserParams]) -> User:
         """
             Creates a new user with the optionally given username and password values
+
+            Optional Arguments:
+                username : str -- username for the new user
+                password : str -- password for the new user
 
             Returns:
                 user : User -- the new user created with the given or default values
@@ -56,12 +60,32 @@ class Factory:
         user.save()
         return user
 
-    def db_create_admin(self, **kwargs : Unpack[UserParams]) -> Admin:
+    def db_create_admin(self, **kwargs : Unpack[FactoryUserParams]) -> Admin:
+        """
+            Creates a new admin user with the optionally given username and password values
+
+            Optional Arguments:
+                username : str -- username for the new user
+                password : str -- password for the new user
+
+            Returns:
+                user : Admin -- the new user created with the given or default values
+        """
         institution = self.db_get_or_create_institution()
         user = self.db_create_user(**kwargs)
         return Admin.objects.create(institution=institution, user=user)
 
-    def db_create_teacher(self, **kwargs : Unpack[UserParams]) -> Teacher:
+    def db_create_teacher(self, **kwargs : Unpack[FactoryUserParams]) -> Teacher:
+        """
+            Creates a new teacher user with the optionally given username and password values
+
+            Optional Arguments:
+                username : str -- username for the new user
+                password : str -- password for the new user
+
+            Returns:
+                user : Teacher -- the new user created with the given or default values
+        """
         institution = self.db_get_or_create_institution()
         user = self.db_create_user(**kwargs)
         teacher = Teacher.objects.create(user=user)
@@ -69,12 +93,28 @@ class Factory:
         teacher.save()
         return teacher
 
-    def db_create_student(self, **kwargs : Unpack[UserParams]) -> Student:
+    def db_create_student(self, **kwargs : Unpack[FactoryUserParams]) -> Student:
+        """
+            Creates a new student user with the optionally given username and password values
+
+            Optional Arguments:
+                username : str -- username for the new user
+                password : str -- password for the new user
+
+            Returns:
+                user : Student -- the new user created with the given or default values
+        """
         institution = self.db_get_or_create_institution()
         user = self.db_create_user(**kwargs)
         return Student.objects.create(user=user, institution=institution)
 
-    def db_create_course_type(self):
+    def db_create_course_type(self) -> CourseType:
+        """
+            Creates a new course type with default values
+
+            Returns:
+                course_type : CourseType -- the new course type with default values
+        """
         defaults = {
             "institution": self.db_get_or_create_institution(),
             "name": "Course Type",
@@ -82,7 +122,17 @@ class Factory:
         }
         return CourseType.objects.create(**defaults)
 
-    def db_create_course(self, **kwargs):
+    def db_create_course(self, **kwargs) -> Course:
+        """
+            Creates a new course with given or generated values
+
+            Optional Arguments:
+                session : Session -- the course's session
+                course_type : CourseTyoe -- the course's type
+
+            Returns:
+                course : Course -- the new course with given or generated values
+        """
         session = kwargs.get("session")
         if not session:
             session = self.db_create_session()
@@ -99,7 +149,13 @@ class Factory:
         }
         return Course.objects.create(**defaults)
 
-    def db_create_session_type(self, **kwargs):
+    def db_create_session_type(self) -> SessionType:
+        """
+            Creates a new session type with default values
+
+            Returns:
+                session_type : SessionType -- the new session type with default values
+        """
         defaults = {
             "institution": self.db_get_or_create_institution(),
             "name": "Session Type",
@@ -107,7 +163,16 @@ class Factory:
         }
         return SessionType.objects.create(**defaults)
 
-    def db_create_session(self, **kwargs):
+    def db_create_session(self, **kwargs) -> Session:
+        """
+            Creates a new session with given or generated values
+
+            Optional Arguments:
+                session_type : SessionType -- the type of the session
+
+            Returns:
+                session : Session -- the new session with given or generated values
+        """
         session_type = kwargs.get("session_type")
         if not session_type:
             session_type = self.db_create_session_type()
@@ -118,19 +183,46 @@ class Factory:
         }
         return Session.objects.create(**defaults)
 
-    def db_create_country(self, **kwargs):
+    def db_create_country(self, **kwargs) -> Country:
+        """
+            Creates new country with given or default values
+
+            Optional Arguments:
+                country : str -- the name of the country
+            
+            Returns:
+                country : Country -- the new country with given or default values
+        """
         defaults = {
             "country": kwargs.get("country", "U.S.A"),
         }
         return Country.objects.create(**defaults)
 
-    def db_create_language(self, **kwargs):
+    def db_create_language(self, **kwargs) -> Language:
+        """
+            Creates new language with given or default values
+
+            Optional Arguments:
+                language : str -- the name of the language
+
+            Returns:
+                language : Language -- the language with given or default values
+        """
         defaults = {
             "language": kwargs.get("language", "klingon"),
         }
         return Language.objects.create(**defaults)
 
-    def db_create_worksheet(self, **kwargs):
+    def db_create_worksheet(self, **kwargs) -> Worksheet:
+        """
+            Creates a new worksheet with given or generated values
+
+            Optional Arguments:
+                course : Course -- the course which will own the worksheet
+
+            Returns:
+                worksheet : Worksheet -- the new worksheet with given or generated values
+        """
         course = kwargs.get("course")
         if not course:
             course = self.db_create_course()
@@ -146,7 +238,17 @@ class Factory:
         }
         return Worksheet.objects.create(**defaults)
 
-    def db_create_expression(self, **kwargs):
+    def db_create_expression(self, **kwargs) -> Expression:
+        """
+            Creates a new expression with given or generated values
+
+            Optional Arguments:
+                worksheet : Worksheet -- the worksheet that owns the expression
+                student : Student -- the student associated with the expression
+
+            Returns:
+                expression: Expression -- the new expression with given or generated values
+        """
         worksheet = kwargs.get("worksheet")
         if not worksheet:
             worksheet = self.db_create_worksheet()
@@ -167,7 +269,7 @@ class Factory:
         }
         return Expression.objects.create(**defaults)
 
-    def db_create_submission(self, **kwargs):
+    def db_create_submission(self, **kwargs) -> StudentSubmission:
         worksheet = kwargs.get("worksheet")
         if not worksheet:
             worksheet = self.db_create_worksheet()
@@ -184,7 +286,7 @@ class Factory:
         return StudentSubmission.objects.create(**defaults)
 
 
-    def db_create_attempt(self, **kwargs):
+    def db_create_attempt(self, **kwargs) -> StudentAttempt:
         expression = kwargs.get("expression")
         if not expression:
             expression = self.db_create_expression()
