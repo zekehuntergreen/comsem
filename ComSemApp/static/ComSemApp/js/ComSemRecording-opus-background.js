5d92226462307c095a0b005f1b83ca954d731e1d
Avery Edmondson <edmondsonavery@gmail.com>
2023-04-04 14:23:06 -0700

    This works. The error message may need changed

--- ComSemApp/static/ComSemApp/js/ComSemRecording-opus-background.js
+++ ComSemApp/static/ComSemApp/js/ComSemRecording-opus-background.js
@@ -7,48 +7,40 @@ var audioBlob;
 var recorder;
 
 function initializeRecorder() {
+    return new Promise((resolve, reject) => {
+        if (!Recorder.isRecordingSupported()) {
+            reject("Recording features are not supported in your browser.");
+        }
 
-    if (!Recorder.isRecordingSupported()) {
-        return console.log("Recording features are not supported in your browser.");
-    }
+        recorder = new Recorder({
+            monitorGain: parseInt(monitorGain.value, 10),
+            numberOfChannels: parseInt(numberOfChannels.value, 10),
+            encoderBitRate: parseInt(bitRate.value, 10),
+            encoderSampleRate: parseInt(encoderSampleRate.value, 10),
+            encoderPath: encoderPath, // declared in templates
+        });
 
-    recorder = new Recorder({
-        monitorGain: parseInt(monitorGain.value, 10),
-        numberOfChannels: parseInt(numberOfChannels.value, 10),
-        encoderBitRate: parseInt(bitRate.value, 10),
-        encoderSampleRate: parseInt(encoderSampleRate.value, 10),
-        encoderPath: encoderPath, // declared in templates
+        recorder.addEventListener("streamError", (e) => {
+            console.error('streamError: ' + e.error.name);
+            if (e.error.name == 'NotAllowedError') {
+                reject("Cannot record. Please allow microphone permissions.")
+            }
+            else {
+                reject(e.error.name);
+            }
+        });
+
+        recorder.addEventListener("streamReady", () => {
+            console.log('Audio stream is ready.');
+            resolve();
+        });
+
+        recorder.addEventListener("dataAvailable", (e) => {
+            // audioBlob is the variable name that should be used to reference the audio in other scripts
+            audioBlob = new Blob([e.detail], { type: 'audio/ogg' });
+            console.log('Audio is ready');
+        });
+
+        recorder.initStream();
     });
-
-    recorder.addEventListener("start", () => {
-        console.log('Recorder is started');
-    });
-
-    recorder.addEventListener("stop", () => {
-        console.log('Recorder is stopped');
-    });
-
-    recorder.addEventListener("pause", () => {
-        console.log('Recorder is paused');
-    });
-
-    recorder.addEventListener("resume", () => {
-        console.log('Recorder is resuming');
-    });
-
-    recorder.addEventListener("streamError", (e) => {
-        console.error('streamError: ' + e.error.name);
-    });
-
-    recorder.addEventListener("streamReady", () => {
-        console.log('Audio stream is ready.');
-    });
-
-    recorder.addEventListener("dataAvailable", (e) => {
-        // audioBlob is the variable name that should be used to reference the audio in other scripts
-        audioBlob = new Blob([e.detail], { type: 'audio/ogg' });
-        console.log('Audio is ready');
-    });
-
-    recorder.initStream();
 }
