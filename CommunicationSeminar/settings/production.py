4ab8279c5384ffa7cae7a08ab231a17b990f99cb
Avery Edmondson <edmondsonavery@gmail.com>
2023-04-13 12:47:25 -0700

    updated so database is usable

--- CommunicationSeminar/settings/production.py
+++ CommunicationSeminar/settings/production.py
@@ -145,8 +145,8 @@ if LIVE:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR + '/db.sqlite3'
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'CommunicationSeminarDjango'
         }
     }
 
